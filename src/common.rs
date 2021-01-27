ed3b828259e7eed95abaf90e99c24a848169e083
Marco Esposito <esposito@imfusion.com>
2021-01-27 19:17:23 +0100

    Added unit test for Executable and Common module

--- src/common.rs
+++ src/common.rs
@@ -49,10 +49,12 @@ pub fn readable_canonical_path<P: AsRef<Path>>(p: P) -> Result<String, LookupErr
             format!("Can't compute canonic path for {:?}", p.as_ref())))?))
 }
 
+/// Shorthand to get some kind of readable representation of a path
 pub fn path_to_string<P: AsRef<Path>>(p: P) -> String {
     p.as_ref().to_str().unwrap_or(format!("{:?}", p.as_ref()).as_ref()).to_owned()
 }
 
+/// Shorthand to get some kind of readable representation of an OsStr
 pub fn osstring_to_string(p: &OsStr) -> String {
     p.to_str().unwrap_or(format!("{:?}", p).as_ref()).to_owned()
 }
@@ -76,4 +78,52 @@ pub fn read_dependencies<P: AsRef<Path> + ?Sized>(path: &P) -> Result<Vec<String
         .filter_map(|s| s.to_str().ok())
         .map(|s| s.to_string())
         .collect::<Vec<String>>())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{LookupError, decanonicalize, readable_canonical_path};
+    use crate::common::read_dependencies;
+    use std::collections::HashSet;
+
+    #[test]
+    fn decanonicalize_removes_prefix() -> Result<(), LookupError> {
+        let cargo_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let cargo_dir_canon = std::fs::canonicalize(&cargo_dir)?;
+
+        let cargo_dir_decanon = decanonicalize(cargo_dir_canon.to_str().unwrap());
+        assert!(!cargo_dir_decanon.contains(r"\\?\"));
+
+        let cargo_dir_decanon2 = readable_canonical_path(&cargo_dir)?;
+        assert!(!cargo_dir_decanon2.contains(r"\\?\"));
+        assert_eq!(cargo_dir_decanon, cargo_dir_decanon2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn read_dependencies_test_exe() -> Result<(), LookupError> {
+        let cargo_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let exe_path = cargo_dir.join("test_data/test_project1/DepRunTest/build-same-output/bin/Debug/DepRunTest.exe");
+
+        let expected_exe_deps: HashSet<String> = ["DepRunTestLib.dll", "VCRUNTIME140D.dll", "ucrtbased.dll", "KERNEL32.dll"]
+            .iter().map(|&s| s.to_owned()).collect();
+        let exe_deps: HashSet<String> = read_dependencies(&exe_path)?.into_iter().collect();
+        assert_eq!(exe_deps, expected_exe_deps);
+
+        Ok(())
+    }
+
+    #[test]
+    fn read_dependencies_test_dll() -> Result<(), LookupError> {
+        let cargo_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let lib_path = cargo_dir.join("test_data/test_project1/DepRunTest/build-same-output/bin/Debug/DepRunTestLib.dll");
+
+        let expected_lib_deps: HashSet<String> = ["KERNEL32.dll", "MSVCP140D.dll", "VCRUNTIME140D.dll", "VCRUNTIME140_1D.dll", "ucrtbased.dll"]
+            .iter().map(|&s| s.to_owned()).collect();
+        let lib_deps: HashSet<String> = read_dependencies(&lib_path)?.into_iter().collect();
+        assert_eq!(lib_deps, expected_lib_deps);
+
+        Ok(())
+    }
 }
--- src/executable.rs
+++ src/executable.rs
@@ -100,3 +100,64 @@ impl Executables {
         sorted_executables
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{LookupError, Executables};
+    use crate::query::LookupQuery;
+    use crate::lookup_path::LookupPath;
+    use crate::runner::Runner;
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
+    use std::ffi::OsStr;
+
+    #[test]
+    fn empty_executables() -> Result<(), LookupError> {
+        let exes = Executables::new();
+        assert!(!exes.contains(OsStr::new("NonExistingExecutable.exe")));
+
+        assert!(exes.get(OsStr::new("NonExistingExecutable.exe")).is_none());
+
+        assert!(exes.get_root()?.is_none());
+
+        assert!(exes.sorted_by_first_appearance().is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    fn executables() -> Result<(), LookupError> {
+        let d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let exe_path = d.join("test_data/test_project1/DepRunTest/build-same-output/bin/Debug/DepRunTest.exe");
+
+        let mut query = LookupQuery::deduce_from_executable_location(&exe_path)?;
+        query.skip_system_dlls = true;
+        let context = LookupPath::new(&query);
+        let mut runner = Runner::new(&query, context);
+        let exes = runner.run()?;
+
+        assert!(exes.contains(OsStr::new("DepRunTest.exe")));
+        assert!(exes.contains(OsStr::new("depruntest.exe")));
+        assert!(!exes.contains(OsStr::new("NonExistingExecutable.exe")));
+
+        assert!(exes.get(OsStr::new("NonExistingExecutable.exe")).is_none());
+        assert!(exes.get(OsStr::new("DepRunTest.exe")).is_some());
+
+        assert_eq!(exes.get_root()?.unwrap().name, "DepRunTest.exe");
+
+        let sorted = exes.sorted_by_first_appearance();
+        let sorted_names: HashSet<&str> = sorted.iter()
+            .filter(|e| e.details.as_ref().map(|d| !d.is_system).unwrap_or(false))
+            .map(|e| e.name.to_str().unwrap()).collect();
+        let expected_names: HashSet<&str> = HashSet::from_iter(
+            ["DepRunTestLib.dll", "DepRunTest.exe",].iter().map(|&s|s));
+        assert_eq!(sorted_names, expected_names);
+
+        let exe_p = exes.get_root()?.unwrap().full_path();
+        assert!(exe_p.is_some());
+        assert_eq!(exe_p.unwrap(), std::fs::canonicalize(exe_path)?);
+
+        Ok(())
+    }
+}
